a00e7a8fcc76c2de5ec2b8d57254df9041977f8e
magyarosim <36436545+magyarosim@users.noreply.github.com>
2018-02-16 10:42:46 +0100

    Update ansible-install.sh

--- ansible-install.sh
+++ ansible-install.sh
@@ -6,3 +6,5 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible
 sudo apt-get install openssh-server
+git clone https://github.com/andreipak/wordpress-ansible.git
+sudo ansible-playbook playbook.yml -i hosts
